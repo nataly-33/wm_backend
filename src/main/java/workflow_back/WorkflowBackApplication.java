60d17b15a7f5f3b63b1f926cf19dca2b22b2fb49
nataly-33 <vanessamartinez1@upb.edu>
2026-04-12 18:21:56 -0400

    commit inicial

---
+++ src/main/java/workflow_back/WorkflowBackApplication.java
@@ -0,0 +1,13 @@
+package workflow_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkflowBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkflowBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/workflow_back/WorkflowBackApplicationTests.java
@@ -0,0 +1,13 @@
+package workflow_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkflowBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
