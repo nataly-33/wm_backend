ae3b64c1c8825be625204a8ee560ae0954c589b4
nataly-33 <vanessamartinez1@upb.edu>
2026-04-14 12:24:29 -0400

    chore: actualizacion

--- src/main/java/workflow_back/WorkflowBackApplication.java
+++ src/main/java/workflow_back/WorkflowBackApplication.java
@@ -2,8 +2,12 @@ package workflow_back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.workflow"})
+@EnableMongoAuditing
+@EnableMongoRepositories(basePackages = {"com.workflow.*.repository"})
 public class WorkflowBackApplication {
 
 	public static void main(String[] args) {
