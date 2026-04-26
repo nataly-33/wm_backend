6bf9ce77d3fd9c7223d3fccb7f09d76fdc0544a6
nataly-33 <vanessamartinez1@upb.edu>
2026-04-26 03:55:13 -0400

    chore: RestTemplate configurado con solo convertidores JSON, eliminando XML

--- src/main/java/com/workflow/config/AppConfig.java
+++ src/main/java/com/workflow/config/AppConfig.java
@@ -2,13 +2,26 @@ package com.workflow.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.ByteArrayHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @Configuration
 public class AppConfig {
 
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        RestTemplate restTemplate = new RestTemplate();
+        // Force JSON-only message converters to prevent XML serialization
+        // (jackson-dataformat-xml on classpath via firebase-admin causes XML to be used)
+        restTemplate.setMessageConverters(List.of(
+                new MappingJackson2HttpMessageConverter(),
+                new StringHttpMessageConverter(),
+                new ByteArrayHttpMessageConverter()
+        ));
+        return restTemplate;
     }
 }
